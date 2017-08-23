13a1323e309b79cdff5ec4ec190f84e8e8ba7c0b
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-08-23 15:47:06 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ var firebase = require("firebase/app");
 var config = {
 	apiKey: "AIzaSyDRRWbzzADN3rhjBWpZeiHfaIq4a1gvIOY",
 	authDomain: "portfolio-83749.firebaseapp.com",
-	databaseURL: "https://portfolio-83749.firebaseio.com/",
+	databaseURL: "https://portfolio-83749.firebaseio.com",
 	projectId: "portfolio-83749",
 	storageBucket: "portfolio-83749.appspot.com",
-	messagingSenderId: "1009089907270"
+	messagingSenderId: "7666199790"
 };
 firebase.initializeApp(config);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App/>, document.getElementById('root'));
 // registerServiceWorker();
