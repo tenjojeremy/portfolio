8f3fd588db456fcfbb5e43356026a2bc2f37a9f2
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-08-23 23:20:45 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import styled from 'styled-components'
 import Intro from './containers/intro';
 import ProjectsData from './projectData.json';
 import screenshot from './images/screenshot.png';
+var Carousel = require('nuka-carousel');
 
 class App extends Component {
+	mixins: [Carousel.ControllerMixin]
 	render() {
 		const MasterContainer = styled.div `
     margin:0;
@@ -23,7 +25,14 @@ class App extends Component {
 		return (
 			<MasterContainer>
 				<Intro/>
-
+				<Carousel>
+				        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
+				        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
+				        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
+				        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
+				        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"/>
+				        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"/>
+				      </Carousel>
 				{projectList}
 			</MasterContainer>
 		);
