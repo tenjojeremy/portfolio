a95941e965b46079238c82497dd2b7fe277b96fb
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-09-05 12:19:16 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
@@ -12,17 +12,21 @@ const store = createStore(Reducers);
 
 // Firebase Configuration
 var config = {
-	apiKey: "AIzaSyDRRWbzzADN3rhjBWpZeiHfaIq4a1gvIOY",
-	authDomain: "portfolio-83749.firebaseapp.com",
-	databaseURL: "https://portfolio-83749.firebaseio.com",
-	projectId: "portfolio-83749",
-	storageBucket: "portfolio-83749.appspot.com",
-	messagingSenderId: "7666199790"
+  apiKey: "AIzaSyDRRWbzzADN3rhjBWpZeiHfaIq4a1gvIOY",
+  authDomain: "portfolio-83749.firebaseapp.com",
+  databaseURL: "https://portfolio-83749.firebaseio.com",
+  projectId: "portfolio-83749",
+  storageBucket: "portfolio-83749.appspot.com",
+  messagingSenderId: "7666199790"
 };
 firebase.initializeApp(config);
 
+//add visit
+firebase.database().ref(`visits`).push({
+	count: 1
+)}
 ReactDOM.render(
-	<Provider store={store}>
-	<App/>
-	</Provider>, document.getElementById('root'));
-// registerServiceWorker();
+  <Provider store={store}>
+  <App/>
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
