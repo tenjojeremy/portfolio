4ca39c2a9c380367f31a2ec7df6b6a80877310b8
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-09-12 10:34:49 -0400

    Update

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,24 @@
-
 // importScripts('/node_modules/workbox-sw/build/workbox-sw.vX.X.X.prod.js');
 
 self.addEventListener('activate', () => {
-  self.clients.matchAll({ type: 'window' }).then(windowClients => {
+  self.clients.matchAll({type: 'window'}).then(windowClients => {
     for (let windowClient of windowClients) {
       windowClient.navigate(windowClient.url);
     }
   });
 });
 
-//Cache Shell
+//Cache Shell - Cache falling back to the network strategy
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll(
-        [
-          '/index.html',
-        ]
-      );
-    })
-  );
+  event.waitUntil(caches.open('v1').then(function(cache) {
+    return cache.addAll(['/index.html']);
+  }).catch((err) => {
+    console.log(err);
+  }));
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(function(response) {
+    return response || fetch(event.request);
+  }));
 });
