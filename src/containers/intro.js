c3af1da41cfe8efac3c5f7e03023866ec9c861ea
tenjojeremy <tenjojeremy@gmail.com>
2018-04-16 19:23:25 -0700

    Update

--- src/containers/intro.js
+++ src/containers/intro.js
@@ -8,21 +8,21 @@ import arrowDownIcon from '../images/arrowDown.svg'
 import { goToAnchor } from 'react-scrollable-anchor'
 
 class name extends React.Component {
-  //initial state
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: 'initial'
-    }
-  }
+	//initial state
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: 'initial'
+		}
+	}
 
-  //Methods
-  ScrollDown = () => {
-    goToAnchor('firstProject', true)
-  }
-  render() {
-    //Properties
-    const rotate360 = keyframes`
+	//Methods
+	ScrollDown = () => {
+		goToAnchor('firstProject', true)
+	}
+	render() {
+		//Properties
+		const rotate360 = keyframes`
 			from {
 				opacity: 0.3;
 			}
@@ -31,135 +31,135 @@ class name extends React.Component {
 				opacity: 1;
 			}
 		`
-    const ArrowDown = styled.img`
-      display: block;
-      margin: 0 auto;
-      width: 40px;
-      cursor: pointer;
-      animation: ${rotate360} 1s linear infinite;
+		const ArrowDown = styled.img`
+			display: block;
+			margin: 0 auto;
+			width: 40px;
+			cursor: pointer;
+			animation: ${rotate360} 1s linear infinite;
 
-      @media (min-width: 600px) {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        margin: auto;
-      }
-    `
-    //Template
-    return (
-      <ParticlesCon>
-        <Particles />
-        <InfoContainer>
-          <Title>Jeremy Tenjo</Title>
-          <SubTitle>Project Showcase</SubTitle>
-          <AboutMe>
-            I am a web developer who enjoys the challenges of solving modern problems by using the latest and greatest
-            web tools at my disposal. I love Javascript, but I am always experimenting with new technologies that make
-            my development more efficient.
-          </AboutMe>
-          <Linktext>Check out my Linkedin, GitHub and Resume</Linktext>
-          <LinksCon>
-            <a
-              title="Linkedin"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://ca.linkedin.com/in/jeremy-tenjo-244048120"
-            >
-              <Icon src={LinkedinIcon} alt="linkedin icon" />
-            </a>
+			@media (min-width: 600px) {
+				position: absolute;
+				bottom: -50px;
+				left: 0;
+				right: 0;
+				margin: auto;
+			}
+		`
+		//Template
+		return (
+			<ParticlesCon>
+				<Particles />
+				<InfoContainer>
+					<Title>Jeremy Tenjo</Title>
+					<SubTitle>Project Showcase</SubTitle>
+					<AboutMe>
+						I am a web developer who enjoys the challenges of solving modern problems by using the latest and greatest
+						web tools at my disposal. I love Javascript, but I am always experimenting with new technologies that make
+						my development more efficient.
+					</AboutMe>
+					<Linktext>Check out my Linkedin, GitHub and Resume</Linktext>
+					<LinksCon>
+						<a
+							title="Linkedin"
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://ca.linkedin.com/in/jeremy-tenjo-244048120"
+						>
+							<Icon src={LinkedinIcon} alt="linkedin icon" />
+						</a>
 
-            <a title="Github" target="_blank" rel="noopener noreferrer" href="https://github.com/tenjojeremy">
-              <Icon src={GithubIcon} alt="github icon" />
-            </a>
-            <a
-              title="Portfolio"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://firebasestorage.googleapis.com/v0/b/portfolio-83749.appspot.com/o/Resume%20-%20Jeremy%20Tenjo.pdf?alt=media&token=45bd1a79-ec56-44b3-acea-c479b8c75d71"
-            >
-              <Icon src={ResumeIcon} alt="CV icon" />
-            </a>
-          </LinksCon>
-          <ArrowDown src={arrowDownIcon} alt="Arrow Down" onClick={this.ScrollDown} />
-        </InfoContainer>
-      </ParticlesCon>
-    )
-  }
+						<a title="Github" target="_blank" rel="noopener noreferrer" href="https://github.com/tenjojeremy">
+							<Icon src={GithubIcon} alt="github icon" />
+						</a>
+						<a
+							title="Portfolio"
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://firebasestorage.googleapis.com/v0/b/portfolio-83749.appspot.com/o/Resume%20-%20Jeremy%20Tenjo.pdf?alt=media&token=45bd1a79-ec56-44b3-acea-c479b8c75d71"
+						>
+							<Icon src={ResumeIcon} alt="CV icon" />
+						</a>
+					</LinksCon>
+					<ArrowDown src={arrowDownIcon} alt="Arrow Down" onClick={this.ScrollDown} />
+				</InfoContainer>
+			</ParticlesCon>
+		)
+	}
 }
 
 //Style
 
 const AboutMe = styled.p`
-  text-align: center;
-  color: white;
-  font-size: 16px;
-  margin-top: 40px;
-  margin-bottom: 40px;
-  line-height: 21px;
-  letter-spacing: 2px;
-  @media (min-width: 600px) {
-    font-size: 18px;
-  }
+	text-align: center;
+	color: white;
+	font-size: 16px;
+	margin-top: 40px;
+	margin-bottom: 40px;
+	line-height: 21px;
+	letter-spacing: 2px;
+	@media (min-width: 600px) {
+		font-size: 18px;
+	}
 `
 const Linktext = styled.p`
-  color: #bdbdbd;
-  text-align: center;
-  font-size: 12px;
+	color: #bdbdbd;
+	text-align: center;
+	font-size: 12px;
 `
 const Icon = styled.img`
-  width: 40px;
-  padding: 10px;
+	width: 40px;
+	padding: 10px;
 `
 const LinksCon = styled.div`
-  ${'' /* background: red; */} height: 70px;
-  width: 180px;
-  margin: 0 auto;
+	${'' /* background: red; */} height: 70px;
+	width: 180px;
+	margin: 0 auto;
 `
 const ParticlesCon = styled.div`
-  height: 100vh;
-  background: #182327;
-  position: relative;
+	height: 100vh;
+	background: #182327;
+	position: relative;
 `
 
 const InfoContainer = styled.div`
-  height: 520px;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  @media (min-width: 600px) {
-    width: 390px;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-  }
+	height: 520px;
+	width: 100%;
+	position: absolute;
+	top: 0;
+	@media (min-width: 600px) {
+		width: 390px;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		margin: auto;
+	}
 `
 const Title = styled.p`
-  color: #049372;
-  font-family: 'PT Serif', serif;
-  font-weight: bolder;
-  font-size: 50px;
-  line-height: 78px;
-  margin-top: 0;
-  margin-bottom: 10px;
-  text-align: center;
-  margin-top: 0;
-  @media (min-width: 600px) {
-    font-size: 66px;
-  }
-  @media (min-width: 950px) {
-    font-size: 66px;
-  }
+	color: #049372;
+	font-family: 'PT Serif', serif;
+	font-weight: bolder;
+	font-size: 50px;
+	line-height: 78px;
+	margin-top: 0;
+	margin-bottom: 10px;
+	text-align: center;
+	margin-top: 0;
+	@media (min-width: 600px) {
+		font-size: 66px;
+	}
+	@media (min-width: 950px) {
+		font-size: 66px;
+	}
 `
 const SubTitle = styled.p`
-  text-align: center;
-  font-size: 20px;
-  line-height: 25px;
-  color: white;
-  font-weight: bold;
+	text-align: center;
+	font-size: 20px;
+	line-height: 25px;
+	color: white;
+	font-weight: bold;
 `
 
 export default name
