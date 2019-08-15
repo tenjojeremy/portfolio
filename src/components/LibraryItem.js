73c23a8f038bc7dcb37bfad0d65123646f2805c4
Jeremy <tenjojeremy@gmail.com>
2019-08-15 09:12:23 -0600

    Yeah

--- src/components/LibraryItem.js
+++ src/components/LibraryItem.js
@@ -1,117 +1,144 @@
-//Vendors
 import React from 'react'
 import styled from 'styled-components'
+import GithubIcon from '../images/github.svg'
+import StorybookIcon from '../images/storybook.svg'
 
-//Components
-
-//Images
-import backgroundIMG from './../images/background.jpg'
 import mainIcon from './../images/FootyCityIcon.svg'
 
-class ComingSoon extends React.Component {
-	//initial state
-	constructor(props) {
-		super(props)
-		this.state = {
-			...props
-		}
-	}
+const ComingSoon = ({ name, logo, repo, site, backgroundIMG }) => {
+  return (
+    <Wrapper backgroundIMG={backgroundIMG}>
+      <Content>
+        <Header>
+          <img src={logo} alt="app" />
+          <p>{name}</p>
+        </Header>
+        <Title>A components and utilitites library</Title>
 
-	//Methods
-	render() {
-		//Properties
+        <Links>
+          <a title="Repo" target="_blank" rel="noopener noreferrer" href={repo}>
+            <Icon src={GithubIcon} alt="github icon" />
+          </a>
 
-		//Template
-		return (
-			<Wrapper>
-				<Content>
-					<Header>
-						<img src={mainIcon} alt="app" />
-						<p>Footy City</p>
-					</Header>
-					<ComingSoonTitle>COMING SOON</ComingSoonTitle>
-				</Content>
-			</Wrapper>
-		)
-	}
+          <a
+            title="Storybook"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={site}
+          >
+            <Icon2 src={StorybookIcon} alt="stirybook icon" />
+          </a>
+        </Links>
+      </Content>
+    </Wrapper>
+  )
 }
 
 //Style
 const Wrapper = styled.div`
-	box-shadow: inset 0px 0px 250px rgba(0, 231, 120, 0.3);
-	background: linear-gradient(108.05deg, rgba(0, 184, 212, 0.9) 0%, rgba(0, 191, 165, 0.9) 100%), url(${backgroundIMG});
-	background-position: center;
-	background-size: cover;
-	background-repeat: no-repeat;
-	width: 100%;
-	height: 200px;
+  box-shadow: inset 0px 0px 250px rgba(0, 231, 120, 0.3);
+  background: linear-gradient(
+      108.05deg,
+      rgba(0, 184, 212, 0.9) 0%,
+      rgba(0, 191, 165, 0.9) 100%
+    ),
+    url(${({ backgroundIMG }) => backgroundIMG});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  display: grid;
+  align-items: center;
+  grid-gap: 20px;
 
-	@media (min-width: 600px) {
-		height: 400px;
-	}
+  @media (min-width: 600px) {
+    height: 400px;
+  }
+`
+
+const Icon = styled.img`
+  width: 30px;
+
+  @media (min-width: 600px) {
+    width: 40px;
+  }
+`
+const Icon2 = styled.img`
+  width: 130px;
+  @media (min-width: 600px) {
+    width: 140px;
+  }
+`
+
+const Links = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: fit-content(100%);
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  grid-gap: 15px;
 `
 
 const Content = styled.div`
-	padding-top: 15px;
-	@media (min-width: 600px) {
-		padding-top: 70px;
-	}
+  display: grid;
+  align-items: center;
+  grid-gap: 20px;
+  padding: 30px;
+
+  @media (min-width: 600px) {
+    padding: 0;
+  }
 `
+
 const Header = styled.div`
-	display: grid;
-	margin: 0 auto;
-	width: 279px;
-	grid-template-columns: auto 200px;
-	grid-gap: 20px;
-	position: relative;
-	height: 112px;
-	@media (min-width: 600px) {
-		width: 450px;
-		grid-template-columns: auto 310px;
-		margin-bottom: 70px;
-	}
+  display: grid;
+  margin: 0 auto;
+  width: fit-content;
+  grid-template-columns: auto 200px;
+  grid-gap: 20px;
+  position: relative;
+  align-items: center;
+  @media (min-width: 600px) {
+    grid-template-columns: fit-content(100%) fit-content(100%);
+  }
 
-	img {
-		width: 60px;
-		height: 100%;
-		@media (min-width: 400px) {
-			width: 70px;
-		}
-		@media (min-width: 600px) {
-			width: 115px;
-		}
-	}
-	p {
-		font-family: HammersmithOne;
-		font-style: normal;
-		font-weight: normal;
-		line-height: normal;
-		font-size: 35px;
-		letter-spacing: 0.13px;
-		color: #ffffff;
-		text-shadow: 0px 0px 9px rgba(255, 255, 255, 0.65);
-		@media (min-width: 600px) {
-			font-size: 65px;
-		}
-	}
+  img {
+    width: 60px;
+    @media (min-width: 400px) {
+      width: 70px;
+    }
+    @media (min-width: 600px) {
+      width: 115px;
+    }
+  }
+  p {
+    font-family: HammersmithOne;
+    font-style: normal;
+    font-weight: normal;
+    line-height: normal;
+    font-size: 35px;
+    letter-spacing: 0.13px;
+    color: #ffffff;
+    white-space: nowrap;
+    text-shadow: 0px 0px 9px rgba(255, 255, 255, 0.65);
+    margin: 0;
+    @media (min-width: 600px) {
+      font-size: 65px;
+    }
+  }
 `
 
-const ComingSoonTitle = styled.p`
-	font-family: HammersmithOne;
-	font-style: normal;
-	font-weight: normal;
-	line-height: normal;
-	letter-spacing: 0.13px;
-	color: #ffffff;
-	text-align: center;
-	margin: 0;
-	width: 100%;
-	font-size: 30px;
-	@media (min-width: 400px) {
-		font-size: 40px;
-	}
-	@media (min-width: 600px) {
-		font-size: 40px;
-	}
+const Title = styled.p`
+  font-family: HammersmithOne;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
+  letter-spacing: 0.13px;
+  color: #ffffff;
+  text-align: center;
+  margin: 0;
+  width: 100%;
+  font-size: 18px;
 `
 export default ComingSoon
--- src/libraries.js
+++ src/libraries.js
@@ -4,6 +4,8 @@ export default [
     logo:
       'https://firebasestorage.googleapis.com/v0/b/tenjo-web-toolkit.appspot.com/o/logo.svg?alt=media&token=c113e410-8f17-4f4d-871f-0427b779e7e9',
     repo: 'https://github.com/tenjojeremy/Web-Toolkit',
-    site: 'https://tenjo-web-toolkit.web.app'
+    site: 'https://tenjo-web-toolkit.web.app',
+    backgroundIMG:
+      'https://images.unsplash.com/photo-1516259680870-b878830674f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80'
   }
 ]
