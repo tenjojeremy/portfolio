346a17ee0ac6a953504b2b7d43b1e9d825f0e07d
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-15 16:19:09 -0500

    ceaned direbae coneifg

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import firebase from 'firebase'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+import './firebase'
 import './index.css'
 import App from './app'
 import Reducers from './state/reducers'
 const store = createStore(Reducers)
-
-const config = {
-  apiKey: 'AIzaSyDRRWbzzADN3rhjBWpZeiHfaIq4a1gvIOY',
-  authDomain: 'portfolio-83749.firebaseapp.com',
-  databaseURL: 'https://portfolio-83749.firebaseio.com',
-  projectId: 'portfolio-83749',
-  storageBucket: 'portfolio-83749.appspot.com',
-  messagingSenderId: '7666199790',
-  appId: '1:7666199790:web:19f33a4fa0d7eb3fa397ff'
-}
-firebase.initializeApp(config)
 
 ReactDOM.render(
   <Provider store={store}>
