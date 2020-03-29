7642c73319d055e8d0b854918398ee2a5f41baa0
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-29 12:43:31 -0400

    cleaned

--- src/.wapp/_extraBuildFiles.js
+++ src/.wapp/_extraBuildFiles.js
@@ -1,6 +1,1 @@
-module.exports = [
-  {
-    from: 'C:\\Dev\\Personal_Projects\\sites\\portfolio/src/fonts',
-    to: 'C:\\Dev\\Personal_Projects\\sites\\portfolio/build/fonts',
-  },
-]
+module.exports = []
