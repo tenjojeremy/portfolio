0f591c705bc61abf6c5b27b3f31f65f437fbecb4
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-30 19:46:44 -0400

    fix caorusel

--- src/pages/home/projects/webapps/webapp/index.js
+++ src/pages/home/projects/webapps/webapp/index.js
@@ -6,6 +6,7 @@ import List from '@tenjojeremy/web-toolkit/dataDisplay/list'
 import Image from '@tenjojeremy/web-toolkit/media/image'
 import Link from '@tenjojeremy/web-toolkit/navigation/link'
 import Button from '@tenjojeremy/web-toolkit/input/button/styles/base'
+import LazyLoad from '@tenjojeremy/web-toolkit/miscUtils/rendering/lazyLoad'
 
 import useCarousel from '../../../../../common/carousel/carousel.state'
 
@@ -27,63 +28,65 @@ const Webapp = ({
   const handleScreenshotClick = (index) => updateCarousel(screenshots, index)
 
   return (
-    <Box
-      styles={styles.wrapper(color, backgroundImage)}
-      isLazyLoaded
-      lazyLoadProps={{ offset: 300 }}
-    >
-      <Box styles={styles.innerWrapper}>
-        <Box styles={styles.infoWrapper}>
-          <Box styles={styles.titlesWrapper}>
-            <Typography styles={styles.titles} text={name} variant='headerTwo' />
-            <Typography
-              styles={{ ...styles.titles, ...styles.position }}
-              text={position}
-              variant='subtitle1'
-            />
-          </Box>
-
-          <Typography styles={styles.description} text={description} variant='body1' />
-
-          <Box styles={styles.techListWrapper}>
-            <Typography styles={styles.techListTitle} text='Tech used:' variant='body1' />
-
-            <Box styles={styles.techList}>
-              {techList.map((tech) => (
-                <Icon
-                  key={tech}
-                  name={`${tech}/1`}
-                  width='auto'
-                  height='20px'
-                  style={styles.icon}
-                  plain
-                />
-              ))}
+    <LazyLoad offset={300}>
+      <Box styles={styles.wrapper(color, backgroundImage)}>
+        <Box styles={styles.innerWrapper}>
+          <Box styles={styles.infoWrapper}>
+            <Box styles={styles.titlesWrapper}>
+              <Typography styles={styles.titles} text={name} variant='headerTwo' />
+              <Typography
+                styles={{ ...styles.titles, ...styles.position }}
+                text={position}
+                variant='subtitle1'
+              />
             </Box>
 
-            <List photoGrid style={{ maxHeight: '200px', overflow: 'auto' }}>
-              {screenshots.map((screenshot, index) => (
-                <Image
-                  key={screenshot}
-                  src={screenshot}
-                  styles={styles.screenshots}
-                  onClick={() => handleScreenshotClick(index)}
-                />
-              ))}
-            </List>
+            <Typography styles={styles.description} text={description} variant='body1' />
 
-            {site && (
-              <Link
-                href={site}
-                styles={{ margin: ['--0 auto', '--0 auto', '--initial'], padding: 'm' }}
-              >
-                <Button text='DEMO' />
-              </Link>
-            )}
+            <Box styles={styles.techListWrapper}>
+              <Typography
+                styles={styles.techListTitle}
+                text='Tech used:'
+                variant='body1'
+              />
+
+              <Box styles={styles.techList}>
+                {techList.map((tech) => (
+                  <Icon
+                    key={tech}
+                    name={`${tech}/1`}
+                    width='auto'
+                    height='20px'
+                    style={styles.icon}
+                    plain
+                  />
+                ))}
+              </Box>
+
+              <List photoGrid style={{ maxHeight: '200px', overflow: 'auto' }}>
+                {screenshots.map((screenshot, index) => (
+                  <Image
+                    key={screenshot}
+                    src={screenshot}
+                    styles={styles.screenshots}
+                    onClick={() => handleScreenshotClick(index)}
+                  />
+                ))}
+              </List>
+
+              {site && (
+                <Link
+                  href={site}
+                  styles={{ margin: ['--0 auto', '--0 auto', '--initial'], padding: 'm' }}
+                >
+                  <Button text='DEMO' />
+                </Link>
+              )}
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </LazyLoad>
   )
 }
 
