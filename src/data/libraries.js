8ad3eff6a7c911ce6ce7eabf7caa8d8c311fa73a
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-30 14:06:52 -0400

    updated icons

--- src/data/libraries.js
+++ src/data/libraries.js
@@ -3,7 +3,7 @@ export default [
     id: 1,
     name: 'Web-Toolkit',
     logo:
-      'https://firebasestorage.googleapis.com/v0/b/tenjo-web-toolkit.appspot.com/o/logo.svg?alt=media&token=75c5c13e-de0b-40f5-b0a0-3045f13a61d7',
+      'https://firebasestorage.googleapis.com/v0/b/tenjo-web-toolkit.appspot.com/o/logo.svg?alt=media&token=0991f39a-0e64-46a5-a097-7ad433798434',
     description: 'A library of React components and utilities',
     repo: 'https://github.com/tenjojeremy/Web-Toolkit',
     site: 'https://tenjo-web-toolkit.web.app',
@@ -13,8 +13,8 @@ export default [
     id: 2,
     name: 'Wapp',
     logo:
-      'https://firebasestorage.googleapis.com/v0/b/wapp-framework.appspot.com/o/logo.png?alt=media&token=76c553fa-2bbc-4e29-9a42-0b32f256bcfd',
-    description: 'React webapp framework',
+      'https://firebasestorage.googleapis.com/v0/b/wapp-framework.appspot.com/o/logo.svg?alt=media&token=af1c4deb-3e67-48ad-bda5-2080a37035dd',
+    description: 'React framework',
     repo: 'https://github.com/tenjojeremy/wapp',
     backgroundColor:
       'linear-gradient(108.05deg, rgba(0, 184, 212, 0.9) 0%, rgba(0, 191, 165, 0.9) 100%)',
